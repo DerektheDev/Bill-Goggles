32ea26b7d205e8e80d314fcb7d1d625db5ec2dc9
Derek Montgomery <derekm@ifit.com>
2017-11-19 17:55:01 -0600

    Initial commit with Charts lib

--- Bill Spin/ViewController.swift
+++ Bill Spin/ViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import Cocoa
+import Charts
 
 class ViewController: NSViewController {
 
+  @IBOutlet weak var pieChart: PieChartView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    pieChartUpdate()
+  }
+  
+  func pieChartUpdate() {
+    let entry1 = PieChartDataEntry(value: Double(7), label: "#1")
+    let entry2 = PieChartDataEntry(value: Double(28), label: "#2")
+    let entry3 = PieChartDataEntry(value: Double(56), label: "#3")
+    let dataSet = PieChartDataSet(values: [entry1, entry2, entry3], label: "Widget Types")
+    let data = PieChartData(dataSet: dataSet)
+    pieChart.data = data
+    pieChart.chartDescription?.text = "Share of Widgets by Type"
+  
+    //All other additions to this function will go here
+  
+    //This must stay at end of function
+    pieChart.notifyDataSetChanged()
   }
 
   override var representedObject: Any? {
---
+++ Podfile
@@ -0,0 +1,14 @@
+# open source
+# source 'https://github.com/CocoaPods/Specs.git'
+
+target 'Bill Spin' do
+  use_frameworks!
+  pod 'Charts'
+  # pod 'ChartsRealm'
+
+  # target 'AppTests' do
+  #   inherit! :search_paths
+  #   pod 'Quick'
+  #   pod 'Nimble'
+  # end
+end
