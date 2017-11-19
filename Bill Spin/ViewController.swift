e57a3fdb28cb975349cb5ae2503386f6128462c2
Derek Montgomery <derekm@ifit.com>
2017-11-19 17:04:00 -0600

    Initial Commit

---
+++ Bill Spin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bill Spin
+//
+//  Created by Derek Montgomery on 11/19/17.
+//  Copyright © 2017 Mighty Ike. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
